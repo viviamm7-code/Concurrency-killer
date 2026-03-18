eb876b0711d457940bdb57d924590925389eaa54
Hojin <robert.ryu01@gmail.com>
2026-03-18 15:40:34 +0900

    feat/project setup

--- src/main/java/com/grape/ticketing/service/MemberService.java
+++ src/main/java/com/grape/ticketing/service/MemberService.java
@@ -1,4 +1,13 @@
 package com.grape.ticketing.service;
 
+import com.grape.ticketing.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
+    private final MemberRepository memberRepository;
 }
