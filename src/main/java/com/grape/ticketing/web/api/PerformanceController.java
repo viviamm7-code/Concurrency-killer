4f1763198f77e9c9b7f4bf6c77dff946a0d8d77b
Lee-Yeonjoo <joos0207@naver.com>
2026-03-19 15:44:28 +0900

    feat: 공연 목록 조회 화면 구현

--- src/main/java/com/grape/ticketing/web/api/PerformanceController.java
+++ src/main/java/com/grape/ticketing/web/api/PerformanceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/performances")
+@RequestMapping("/api/performances")
 @RequiredArgsConstructor
 public class PerformanceController {
     private final PerformanceService performanceService;
