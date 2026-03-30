bfb61686bb1f9ccc657be2e3b247d68b6f05fe53
jinsung <viviamm7@gmail.com>
2026-03-30 17:35:35 +0900

    feat Spring Security랑 Sesstion 환경설정 충돌 해결

---
+++ src/main/java/com/grape/ticketing/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.grape.ticketing.service;
+
+import com.grape.ticketing.domain.member.Member;
+import com.grape.ticketing.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("유저 없음"));
+
+        return User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+                .authorities(member.getRole()) // ROLE_USER
+                .build();
+    }
+}
